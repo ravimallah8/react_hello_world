bc9eba559143fcf63db40aa2faa384a366f72a6c
Ravi Mallah <ravimallah8@gmail.com>
2021-08-11 00:00:06 +0530

    SetState

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react'
 
 class Counter extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            count: 0
+        }
+    }
+    increment(){
+        this.setState(
+            (prevState) => ({
+                count: prevState.count + 1
+            }),
+            () => {
+                console.log("Callback value : ", this.state.count)
+            }
+        )
+        console.log(this.state.count)
+    }
+    incrementThree(){
+        this.increment()
+        this.increment()
+        this.increment()
+    }
     render() {
         return (
             <div>
-                count
+                <h2>Count - {this.state.count}</h2>
+                <button onClick={() => this.incrementThree()}>increment</button>
             </div>
         )
     }
