43a6684e4880d7656809b7b37a309350a6ff93de
Ravi Mallah <ravimallah8@gmail.com>
2021-08-11 12:46:44 +0530

    Binding Event Handler

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import Message from './components/Message';
 import Counter from './components/Counter';
 import FunctionClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
 
 function App() {
   return (
     <div className="App">
-      <FunctionClick></FunctionClick>
-      <ClassClick></ClassClick>
+      <EventBind/>
+      {/* <FunctionClick></FunctionClick> */}
+      {/* <ClassClick></ClassClick> */}
       { /*<Counter /> */}
       { /*<Message /> */}
       {/* <Welcome name="Ravi" heroName="VisualMatrix"></Welcome> */}
